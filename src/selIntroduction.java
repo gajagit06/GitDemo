8770d8da251a731ed796cf02c172de8599e1f51d
gaja <gajalakshmi.jothimani@gmail.com>
2021-12-27 21:54:51 -0500

    first commit

---
+++ src/selIntroduction.java
@@ -0,0 +1,16 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class selIntroduction {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.setProperty("webdriver.chrome.driver","C:\\Users\\gowri\\OneDrive\\Documents\\chromedriver.exe");
+		WebDriver driver=new ChromeDriver();
+		
+		
+
+	}
+
+}
+  
